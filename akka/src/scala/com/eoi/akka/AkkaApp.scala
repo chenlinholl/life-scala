485122035047106ce4d285abc8a15e1c781c39fc
tao.zeng <tao.zeng9@gmail.com>
2018-05-24 09:35:47 +0800

    akka

--- akka/src/scala/com/eoi/akka/AkkaApp.scala
+++ akka/src/scala/com/eoi/akka/AkkaApp.scala
@@ -1,6 +1,6 @@
 package com.eoi.akka
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorSystem, Props}
 import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpResponse
@@ -23,7 +23,21 @@ object AkkaApp {
 
 
   def main(args: Array[String]): Unit = {
-    Http().bindAndHandle(new IndexRoute().route, "0.0.0.0", 8088, log = log)
+    // Http().bindAndHandle(new IndexRoute().route, "0.0.0.0", 8088, log = log)
+
+    // 缺省的Actor构造函数
+    val helloActor = system.actorOf(Props[HelloActor])
+    helloActor.!("hello")
+    helloActor ! "喂"
+    helloActor.!(123)
+  }
+
+  class HelloActor extends Actor {
+    override def receive: PartialFunction[Any, Unit] = {
+      case "hello" => println("您好！")
+      case "喂" => println("喂，您好！")
+      case _ => println("您是?")
+    }
   }
 
   implicit def globalExceptionHandler: ExceptionHandler =
